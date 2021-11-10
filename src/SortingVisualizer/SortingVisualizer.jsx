3bb9d218e83f05ee4ffd99c9d98eac33c5a4495e
WesleyM314 <wam34@pitt.edu>
2021-11-09 23:23:18 -0500

    Animation working

---
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,138 @@
+import React from "react";
+import Gradient from "javascript-color-gradient";
+import "./SortingVisualizer.css";
+import "../sortingAlgorithms/sortingAlgorithms.js";
+import {
+	bubbleSortAnimations,
+	heapSortAnimations,
+	selectionSortAnimations,
+} from "../sortingAlgorithms/sortingAlgorithms.js";
+
+// Animation speed
+const ANIMATION_SPEED_MS = 0.5;
+
+// Bars in array
+const NUM_ARRAY_BARS = 400;
+
+const NUM_COLORS = 100;
+const colorGradient = new Gradient();
+colorGradient.setGradient(
+	"#ff0000",
+	"#ff6600",
+	"fff200",
+	"10ff00",
+	"00e1ff",
+	"000cff",
+	"ae00ff"
+);
+colorGradient.setMidpoint(NUM_COLORS);
+const colorArr = colorGradient.getArray();
+// console.log(colorArr);
+
+export class SortingVisualizer extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			array: [],
+		};
+	}
+
+	componentDidMount() {
+		this.resetArray();
+	}
+
+	resetArray() {
+		const array = [];
+		for (let i = 0; i < NUM_ARRAY_BARS; i++) {
+			array.push(randomIntInRange(0, NUM_COLORS - 1));
+			// array.push(randColor());
+		}
+		this.setState({ array });
+	}
+
+	bubbleSort() {
+		const animations = bubbleSortAnimations(this.state.array);
+		// console.log(animations);
+		// Go through animations
+		for (let i = 0; i < animations.length; i++) {
+			const arrBars = document.getElementsByClassName("array-bar");
+			const [idx, val] = animations[i];
+			const barStyle = arrBars[idx].style;
+			const color = colorArr[val]; // New color
+			setTimeout(() => {
+				barStyle.backgroundColor = color;
+			}, i * ANIMATION_SPEED_MS);
+		}
+	}
+
+	selectionSort() {
+		const animations = selectionSortAnimations(this.state.array);
+		// Go through animations
+		for (let i = 0; i < animations.length; i++) {
+			const arrBars = document.getElementsByClassName("array-bar");
+			const [idx, val] = animations[i];
+			const barStyle = arrBars[idx].style;
+			const color = colorArr[val]; // New color
+			setTimeout(() => {
+				barStyle.backgroundColor = color;
+			}, i * ANIMATION_SPEED_MS);
+		}
+	}
+
+	heapSort() {
+		const animations = heapSortAnimations(this.state.array);
+		// Go through animations
+		for (let i = 0; i < animations.length; i++) {
+			const arrBars = document.getElementsByClassName("array-bar");
+			const [idx, val] = animations[i];
+			const barStyle = arrBars[idx].style;
+			const color = colorArr[val]; // New color
+			setTimeout(() => {
+				barStyle.backgroundColor = color;
+			}, i * ANIMATION_SPEED_MS);
+		}
+	}
+
+	sortTest() {}
+
+	render() {
+		const { array } = this.state;
+
+		return (
+			<>
+				<div className="button-row">
+					<button onClick={() => this.resetArray()}>Randomize</button>
+					<button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+					<button onClick={() => this.selectionSort()}>Selection Sort</button>
+					<button onClick={() => this.heapSort()}>Heap Sort</button>
+				</div>
+				<div className="array-container">
+					{array.map((value, idx) => (
+						<div
+							className="array-bar"
+							key={idx}
+							style={{
+								backgroundColor: colorArr[value],
+								// height: `${75}vh`,
+							}}
+						></div>
+					))}
+				</div>
+			</>
+		);
+	}
+}
+
+/**
+ * Returns a random int from [min, max] inclusive
+ * @param {min} min Minimum bound
+ * @param {max} max Maximum bound
+ */
+function randomIntInRange(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// function randColor() {
+// 	return Math.floor(Math.random() * 0xffffff);
+// }
---
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -0,0 +1,120 @@
+export const bubbleSortAnimations = array =>
+{
+	const animations = [];
+	const arrCopy = array.slice();
+
+	// Sort
+	let len = arrCopy.length;
+	let i, j;
+	for (i = 0; i < len - 1; i++)
+	{
+		for (j = 0; j < len - i - 1; j++)
+		{
+			// console.log(`i: ${i}, j: ${j}`);
+			if (arrCopy[j] > arrCopy[j + 1])
+			{
+				// Record animations
+				// Set bar at index j to value at j+1
+				animations.push([j, arrCopy[j + 1]]);
+				// Set bar at index j+1 to value at j
+				animations.push([j + 1, arrCopy[j]]);
+
+				swap(arrCopy, j, j + 1);
+			}
+		}
+	}
+	// console.log(arrCopy);
+	console.log("bubble sort done");
+	return animations;
+}
+
+export const selectionSortAnimations = array =>
+{
+	const animations = [];
+	const arrCopy = array.slice();
+	let len = arrCopy.length;
+
+	let i, j, minIdx;
+	for (i = 0; i < len - 1; i++)
+	{
+		// Find min value in unsorted portion
+		minIdx = i;
+		for (j = i + 1; j < len; j++)
+		{
+			if (arrCopy[j] < arrCopy[minIdx])
+				minIdx = j;
+		}
+		// Record animations
+		animations.push([minIdx, arrCopy[i]]);
+		animations.push([i, arrCopy[minIdx]]);
+		// Swap minimum found value
+		swap(arrCopy, minIdx, i);
+	}
+	console.log("selection sort done");
+	return animations;
+}
+
+export const heapSortAnimations = array =>
+{
+	const animations = [];
+	const arrCopy = array.slice();
+	let len = arrCopy.length;
+
+	// console.log("Unsorted");
+	// console.log(arrCopy);
+
+	// Build heap
+	for (let i = Math.floor(len / 2) - 1; i >= 0; i--)
+	{
+		heapify(animations, arrCopy, len, i);
+	}
+
+	// Extract from heap one at a time
+	for (let i = len - 1; i > 0; i--)
+	{
+		// Move current root to end
+		animations.push([0, arrCopy[i]]);
+		animations.push([i, arrCopy[0]]);
+		swap(arrCopy, 0, i);
+		// Heapify reduced heap
+		heapify(animations, arrCopy, i, 0);
+	}
+	// console.log("Sorted");
+	// console.log(arrCopy);
+	return animations;
+}
+
+// Heapify a subtree rooted with node i
+function heapify(animations, arr, len, i) {
+	let largest = i;	// Init largest as root
+	let left = 2 * i + 1;	// Left
+	let right = 2 * i + 2;	// Right
+
+	// If left larger than root
+	if (left < len && arr[left] > arr[largest])
+	{
+		largest = left;
+	}
+	// If right larger than current largest
+	if (right < len && arr[right] > arr[largest])
+	{
+		largest = right;
+	}
+	// If largest is not root, swap
+	if (largest !== i)
+	{
+		animations.push([i, arr[largest]]);
+		animations.push([largest, arr[i]]);
+		swap(arr, i, largest);
+
+		// Recursively heapify the altered sub-tree
+		heapify(animations, arr, len, largest);
+	}
+}
+
+function swap(arr, a, b)
+{
+	let temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
+}
