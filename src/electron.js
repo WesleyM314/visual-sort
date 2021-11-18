e6220e6a29c128ef3d415e91110f78db35920a84
WesleyM314 <wam34@pitt.edu>
2021-11-18 18:53:07 -0500

    Package as electron app

--- src/electron.js
+++ src/electron.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const path = require('path');
+const url = require('url');
 
 let mainWindow;
 
